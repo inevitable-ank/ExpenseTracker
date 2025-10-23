a9a57b8b65304122e8098b88e7a0b5ff8ccec151
inevitable-ank <ankitranjanyadav@gmail.com>
2025-10-23 22:02:10 +0530

    Added the backend link

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,10 +7,7 @@ import GridBackground from "./components/ui/GridBackgroun.jsx";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-	// TODO => Update the uri on production
-	uri: import.meta.env.VITE_NODE_ENV === "development" 
-		? "http://localhost:5000/graphql" 
-		: "/graphql", // Vercel will route this to /api/graphql
+	uri: import.meta.env.VITE_GRAPHQL_URL || "http://localhost:5000/graphql",
 	cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
 	credentials: "include", // This tells Apollo Client to send cookies along with every request to the server.
 });
